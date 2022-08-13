f73315f3923f5fcb907705507ce5b1f91ebb3da1
T-Lind <tiernanxklindauer@gmail.com>
2022-08-13 15:08:49 -0500

    CSV reader class is completely good

--- src/main/java/DataCSVRead.java
+++ src/main/java/DataCSVRead.java
@@ -1,67 +1,24 @@
+import ml.dmlc.xgboost4j.java.Rabit;
 import org.tribuo.*;
 import org.tribuo.classification.Label;
-import org.tribuo.classification.LabelFactory;
-import org.tribuo.data.columnar.FieldExtractor;
-import org.tribuo.data.columnar.FieldProcessor;
-import org.tribuo.data.columnar.ResponseProcessor;
-import org.tribuo.data.columnar.RowProcessor;
-import org.tribuo.data.columnar.extractors.DoubleExtractor;
-import org.tribuo.data.columnar.extractors.FloatExtractor;
 import org.tribuo.data.columnar.processors.field.DoubleFieldProcessor;
-import org.tribuo.data.columnar.processors.response.FieldResponseProcessor;
-import org.tribuo.data.csv.CSVDataSource;
-import org.tribuo.data.csv.CSVLoader;
-import org.tribuo.data.text.impl.BasicPipeline;
-import org.tribuo.evaluation.TrainTestSplitter;
-import org.tribuo.math.optimisers.AdaGrad;
-import org.tribuo.math.optimisers.SGD;
-import org.tribuo.regression.RegressionFactory;
-import org.tribuo.regression.Regressor;
-import org.tribuo.regression.evaluation.RegressionEvaluator;
-import org.tribuo.regression.rtree.CARTRegressionTrainer;
-import org.tribuo.regression.sgd.linear.LinearSGDTrainer;
-import org.tribuo.regression.sgd.objectives.SquaredLoss;
-import org.tribuo.regression.xgboost.XGBoostRegressionTrainer;
-import org.tribuo.util.Util;
-import org.tribuo.util.tokens.impl.BreakIteratorTokenizer;
+import support.CSVDataReader;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Locale;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-
 /**
  * A runner class to experiment with loading data. Loads an external CSV file of wine data.
  */
 public class DataCSVRead {
     public static void main(String[] args) throws IOException {
-        // Get the entire dataset
-        //var csvPath = Paths.get("C:\\Users\\zenith\\Downloads\\wine-quality-master\\wine-quality-master\\winequality\\winequality-red.csv");
-        var csvPath = Paths.get("C:\\Users\\zenith\\Downloads\\data.csv");
 
+        CSVDataReader reader = new CSVDataReader("C:\\Users\\zenith\\Downloads\\data.csv");
+        reader.generateDataColumn("data", Double.class);
 
-        // Read all lines from the dataset
-        var csvLines = Files.readAllLines(csvPath, StandardCharsets.UTF_8);
+        var data = reader.getDataColumn("data");
 
-
-        var responseProcessor = new FieldResponseProcessor("data","UNK",new LabelFactory());
-
-        var fieldProcessors = new HashMap<String, FieldProcessor>();
-        fieldProcessors.put("data", new DoubleFieldProcessor("data"));
-
-        var rowProcessor = new RowProcessor<Label>(responseProcessor, fieldProcessors);
-
-
-        var csvSource = new CSVDataSource<Label>(csvPath,rowProcessor,true);
-        var datasetFromCSV = new MutableDataset<Label>(csvSource);
-
-        System.out.println(datasetFromCSV.getExample(1));
+        System.out.println(reader.getData("data", 1));
     }
 
     public static void printExample(Example<Label> e) {
--- src/main/java/support/CSVDataReader.java
+++ src/main/java/support/CSVDataReader.java
@@ -1,4 +1,85 @@
 package support;
 
+
+import ml.dmlc.xgboost4j.java.Rabit;
+import org.tensorflow.proto.framework.DataType;
+import org.tribuo.Example;
+import org.tribuo.Feature;
+import org.tribuo.MutableDataset;
+import org.tribuo.classification.Label;
+import org.tribuo.classification.LabelFactory;
+import org.tribuo.data.columnar.FieldProcessor;
+import org.tribuo.data.columnar.RowProcessor;
+import org.tribuo.data.columnar.processors.field.DoubleFieldProcessor;
+import org.tribuo.data.columnar.processors.field.TextFieldProcessor;
+import org.tribuo.data.columnar.processors.response.FieldResponseProcessor;
+import org.tribuo.data.csv.CSVDataSource;
+import org.tribuo.data.text.impl.BasicPipeline;
+import org.tribuo.util.tokens.impl.BreakIteratorTokenizer;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 public class CSVDataReader {
+    private Path csvPath;
+
+    private List<String> csvLines;
+
+    private HashMap<String, MutableDataset<Label>> generatedData;
+
+    public CSVDataReader(String csvPath) throws IOException {
+        this.csvPath = Paths.get(csvPath);
+        csvLines = Files.readAllLines(this.csvPath, StandardCharsets.UTF_8);
+
+        generatedData = new HashMap<>();
+    }
+
+    public void generateDataColumn(String name, Object dataType){
+        var responseProcessor = new FieldResponseProcessor(name,"UNK",new LabelFactory());
+
+        FieldProcessor processor;
+
+
+        if(dataType == Double.class){
+            processor = new DoubleFieldProcessor(name);
+        }
+        else if(dataType == String.class){
+            var textPipeline = new BasicPipeline(new BreakIteratorTokenizer(Locale.US),2);
+            processor = new TextFieldProcessor(name, textPipeline);
+        }
+        else{
+            throw new RuntimeException("Improper dataType specified in CSVDataReader.generateDataColumn()");
+        }
+
+        var fieldProcessors = new HashMap<String, FieldProcessor>();
+        fieldProcessors.put(name, processor);
+
+        var rowProcessor = new RowProcessor<Label>(responseProcessor, fieldProcessors);
+
+        var csvSource = new CSVDataSource<Label>(csvPath,rowProcessor,true);
+
+        generatedData.put(name, new MutableDataset<Label>(csvSource));
+    }
+
+
+    public MutableDataset<Label> getDataColumn(String name){
+        return generatedData.get(name);
+    }
+
+    public Label getData(String columnName, int index){
+        return getOutput(getDataColumn(columnName).getExample(index));
+    }
+
+    private Label getOutput(Example<Label> e) {
+        return e.getOutput();
+    }
 }
